6589a6e2ccd0baa3ae2112059fec04a88b865a08
KimMinSub <umnt6626@gmail.com>
2023-01-02 21:54:22 +0900

    프로젝트 생성, bootstrap.css 추가

---
+++ src/main/java/toyProjectBundles/ToyProjectBundlesApplication.java
@@ -0,0 +1,13 @@
+package toyProjectBundles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProjectBundlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyProjectBundlesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toyProjectBundles/ToyProjectBundlesApplicationTests.java
@@ -0,0 +1,13 @@
+package toyProjectBundles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProjectBundlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
